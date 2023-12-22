2cdc01309673626301eaeda8de830c0e3bb0c044
Pawat13499 <pawat13499@gmail.com>
2023-12-23 00:38:43 +0700

    เพิ่ม23-12-66

--- Ex_Syntax.java
+++
@@ -1,5 +0,0 @@
-public class Ex_Syntax {
-    public static void main(String[] args) {
-        System.out.println("");
-    }
-}
---
+++ Ex_Type.java
@@ -0,0 +1,5 @@
+public class Ex_Type {
+    public static void main(String[] args) {
+        int a = 10;
+    }
+}
