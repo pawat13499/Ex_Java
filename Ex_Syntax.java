1cc6fc6fd2e2c0f2bdecc9e7a26ad7d52b89589c
Pawat13499 <pawat13499@gmail.com>
2023-12-23 00:27:04 +0700

    เพิ่ม01

--- Ex_Syntax.java
+++
@@ -1,9 +0,0 @@
-public class Ex_Syntax {
-
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello02");
-        System.out.println("Hello03");
-        System.out.println("มากับอีกไฟล๋");
-    }
-}
--- Ex_type.java
+++
@@ -1,8 +0,0 @@
-public class Ex_type {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int c = a + b;
-        System.out.println(c);
-    }
-}
