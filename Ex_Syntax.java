462ad45d426c65ff5ed22a68231c14db63e3f873
Pawat13499 <pawat13499@gmail.com>
2023-12-23 00:18:32 +0700

    มาใหม่03

--- Ex_Syntax.java
+++ Ex_Syntax.java
@@ -3,5 +3,6 @@ public class Ex_Syntax {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello02");
+        System.out.println("Hello03");
     }
 }
